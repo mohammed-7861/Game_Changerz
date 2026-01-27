ded06a1809a8dd648055ed370c8247c124b2f933
mohammed-7861 <msaiger72@gmail.com>
2026-01-26 22:55:49 -0800

    Move images to public folder and update Header logo path
    Deleted IMG_0960.jpeg, moved hairy.png to the public folder, and updated the Header component to reference the new image path for the logo.

--- game-changers/src/components/Header.tsx
+++ game-changers/src/components/Header.tsx
@@ -20,7 +20,7 @@ const Header = () => {
           {/* Logo Container - Left Side */}
           <div className="flex-shrink-0 flex flex-col items-center gap-2">
             <img
-              src="/hairy.png"
+              src="public/hairy.png"
               alt="Game Changerz Logo"
               className="h-24 md:h-[120px] w-auto object-contain"
             />
